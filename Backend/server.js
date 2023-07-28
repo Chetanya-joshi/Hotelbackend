edfd6e244b046466a22c7aca4bee5d1ddc70a573
Chetanya-joshi <108217245+Chetanya-joshi@users.noreply.github.com>
2023-07-28 12:13:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,7 @@ app.use(cors())
 
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "../frontend/build")))
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
-})
+
 
 
 
